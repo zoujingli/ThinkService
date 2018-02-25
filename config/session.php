2fb048fe911cef31951bc121dd580ab0ee5dc3b4
Anyon <zoujingli@qq.com>
2018-02-25 16:02:33 +0800

    [更新]debug网页授权

--- config/session.php
+++ config/session.php
@@ -15,6 +15,7 @@
 /* 定义Session会话字段名 */
 $session_name = 's' . substr(md5(__DIR__), -8);
 $session_path = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
+file_exists($session_path) || mkdir($session_path, 0755, true);
 
 /* 定义Session会话参数 */
 return [
