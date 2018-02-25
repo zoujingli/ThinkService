776654457babbcd0502c8373026895d885d199bd
Anyon <zoujingli@qq.com>
2018-02-25 16:01:27 +0800

    [更新]debug网页授权

--- config/session.php
+++ config/session.php
@@ -14,13 +14,15 @@
 
 /* 定义Session会话字段名 */
 $session_name = 's' . substr(md5(__DIR__), -8);
+$session_path = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
 
+/* 定义Session会话参数 */
 return [
     'id'             => '',
     'type'           => '',
     'prefix'         => 'ta',
     'auto_start'     => true,
-    'path'           => env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR,
+    'path'           => $session_path,
     'name'           => $session_name,
     'var_session_id' => $session_name,
 ];
