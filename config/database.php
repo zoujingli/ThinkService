161dbbaa0e457da80971eef90d5f2f67764d091e
Anyon <zoujingli@qq.com>
2018-03-14 15:45:35 +0800

    [更新]修改项目配置

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'Test12345',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
