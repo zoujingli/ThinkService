7824094abd7c58027e6290eee9e8f34953eeab01
Anyon <zoujingli@qq.com>
2018-03-14 15:44:06 +0800

    [更新]同步项目配置

--- application/tags.php
+++ application/tags.php
@@ -12,36 +12,28 @@
 // | github开源项目：https://github.com/zoujingli/ThinkService
 // +----------------------------------------------------------------------
 
-use think\Db;
-use think\Exception\HttpResponseException;
+namespace think;
+
+use think\exception\HttpResponseException;
 
 return [
-    //控制器开始前，进行权限检查
+    // 控制器开始前，进行权限检查
     'action_begin' => function () {
         $request = app('request');
         list($module, $controller, $action) = [$request->module(), $request->controller(), $request->action()];
         $node = strtolower("{$module}/{$controller}/{$action}");
-        $info = Db::name('SystemNode')->where(['node' => $node])->find();
+        $info = Db::name('SystemNode')->cache(true, 30)->where(['node' => $node])->find();
         $access = ['is_menu' => intval(!empty($info['is_menu'])), 'is_auth' => intval(!empty($info['is_auth'])), 'is_login' => empty($info['is_auth']) ? intval(!empty($info['is_login'])) : 1];
         // 登录状态检查
         if (!empty($access['is_login']) && !session('user')) {
-            if ($request->isAjax()) {
-                throw new HttpResponseException(json(['code' => 0, 'msg' => '抱歉，您还没有登录获取访问权限！', 'url' => url('@admin/login')]));
-            }
-            throw new HttpResponseException(redirect('@admin/login'));
+            $msg = ['code' => 0, 'msg' => '抱歉，您还没有登录获取访问权限！', 'url' => url('@admin/login')];
+            throw new HttpResponseException($request->isAjax() ? json($msg) : redirect($msg['url']));
         }
         // 访问权限检查
         if (!empty($access['is_auth']) && !auth($node)) {
             throw new HttpResponseException(json(['code' => 0, 'msg' => '抱歉，您没有访问该模块的权限！']));
         }
         // 模板常量声明
-        $appRoot = $request->root();
-        $uriRoot = rtrim(preg_match('/\.php$/', $appRoot) ? dirname($appRoot) : $appRoot, '/');
-        app('view')->init(config('template.'))->config('tpl_replace_string', [
-            '__APP__'    => $appRoot,
-            '__ROOT__'   => $uriRoot,
-            '__SELF__'   => $request->url(),
-            '__STATIC__' => $uriRoot . "/static",
-        ])->assign('classuri', "{$module}/{$controller}");
+        app('view')->init(config('template.'))->assign(['classuri' => "{$module}/{$controller}"]);
     },
 ];
--- config/template.php
+++ config/template.php
@@ -12,21 +12,34 @@
 // | github开源项目：https://github.com/zoujingli/ThinkService
 // +----------------------------------------------------------------------
 
+use think\Db;
+use think\Exception\HttpResponseException;
+
+// 模板常量声明
+$appRoot = app('request')->root();
+$uriRoot = rtrim(preg_match('/\.php$/', $appRoot) ? dirname($appRoot) : $appRoot, '\\/');
+
 return [
     // 模板引擎类型 支持 php think 支持扩展
-    'type'         => 'Think',
+    'type'               => 'Think',
     // 模板路径
-    'view_path'    => '',
+    'view_path'          => '',
     // 模板后缀
-    'view_suffix'  => 'html',
+    'view_suffix'        => 'html',
     // 模板文件名分隔符
-    'view_depr'    => DIRECTORY_SEPARATOR,
+    'view_depr'          => DIRECTORY_SEPARATOR,
     // 模板引擎普通标签开始标记
-    'tpl_begin'    => '{',
+    'tpl_begin'          => '{',
     // 模板引擎普通标签结束标记
-    'tpl_end'      => '}',
+    'tpl_end'            => '}',
     // 标签库标签开始标记
-    'taglib_begin' => '{',
+    'taglib_begin'       => '{',
     // 标签库标签结束标记
-    'taglib_end'   => '}',
+    'taglib_end'         => '}',
+    // 定义模板替换字符串
+    'tpl_replace_string' => [
+        '__APP__'    => $appRoot,
+        '__ROOT__'   => $uriRoot,
+        '__STATIC__' => $uriRoot . "/static",
+    ],
 ];
